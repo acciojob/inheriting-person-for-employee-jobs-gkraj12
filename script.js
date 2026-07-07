6c6d60b9b00390a375bdaa85f7b0b4d5a3eb0ed3
gaurav kumar <gauravkumarskg851215@gmail.com>
2026-07-07 17:35:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-class Person {
+ class Person {
   constructor(name, age) {
     this.name = name;
     this.age = age;
   }
 
   greet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
   }
 }
 
@@ -16,9 +16,12 @@ class Employee extends Person {
   }
 
   jobGreet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    console.log(
+      `Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+    );
   }
 }
 
+// Do not change the code below this line
 window.Person = Person;
 window.Employee = Employee;
